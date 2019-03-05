f816ba21c6424b463194ecb32d0134f5c1092ba1
JAIN, PIYUSH <piyush.jain@tuv-sud.sg>
2019-03-06 00:54:43 +0800

    config changed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   consumer_key: "wZuK0jNgrtTYM0jIFHbxDGheO",
   consumer_secret: "E82ax3gCwLOsHhp84hYs8RoZeYdzZRFn57vDk4tswRdoZ13GmX",
-  access_token: "49905062-Lhzqry0ewCjUx7kydSrxJaynVuAmaQxCDb4t9iw09",
+  access_token_key: "49905062-Lhzqry0ewCjUx7kydSrxJaynVuAmaQxCDb4t9iw09",
   access_token_secret: "bEGZIvoNGaGlWe65jjQP14tJmEuRcpKlqgFQUMgny23aX"
 };
